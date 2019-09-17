30736aeefde9b673ca1c342037f366baac9357f5
Jeff Hanna <jeff.b.hanna@gmail.com>
2019-09-16 20:45:53 -0500

    NACA_5 is working!

--- naca.py
+++ naca.py
@@ -1,11 +1,10 @@
-"""[summary]
-
-Returns:
-	 [type] -- [description]
+"""
+[summary]
 """
 
 import abc
 from collections import namedtuple
+import math
 import matplotlib.pyplot # FOR TESTING ONLY. TO BE REMOVED WHEN THIS LIBRARY GOES INTO PRODUCTION.
 import numpy
 
@@ -38,7 +37,7 @@ class NACA_Base( abc.ABC ):
 
 	@property
 	def naca_number( self ):
-		''' '''
+		'''Getter for the NACA number being used as the input to this class.'''
 		return self._naca_number
 
 	@property
@@ -76,16 +75,12 @@ class NACA_Base( abc.ABC ):
 		[summary]
 		"""
 
-		pass
-
 
 	@abc.abstractmethod
 	def _mean_camber_line( self ):
 		"""
 		[summary]
 		"""
-
-		pass
 
 
 
@@ -141,7 +136,8 @@ class NACA_4( NACA_Base ):
 
 		x_pos = ( self._x_points - yt * numpy.sin( th ), yc + yt * numpy.cos( th ) )
 		y_pos = ( self._x_points + yt * numpy.sin( th ), yc - yt * numpy.cos( th ) )
-		return ( x_pos, y_pos )
+
+		return( x_pos, y_pos )
 
 
 	def _dyc_over_dx( self ):
@@ -203,41 +199,72 @@ class NACA_5( NACA_Base ):
 
 		self._mean_line_map = { 0.05 : Mean_Line_Data( m = 0.0580, k1 = 361.400 ),
 										0.10 : Mean_Line_Data( m = 0.1260, k1 = 51.640 ),
-										0.15 : Mean_Line_Data( m = 0.2025, k1 = 15.957 ),
+										0.155 : Mean_Line_Data( m = 0.2025, k1 = 15.957 ),
 										0.20 : Mean_Line_Data( m = 0.2900, k1 = 6.643 ),
 										0.25 : Mean_Line_Data( m = 0.3910, k1 = 3.230 ), }
 
+		self._a = ( 0.2969, -0.1260, -0.3516, 0.2843, -0.1036 )
+
 		# LPSTT
-		# NACA 23112 profile describes an airfoil with design lift coefficient of 0.3 (0.15*2), the point of maximum camber located at 15% chord (5*3), reflex camber (1), and maximum thickness of 12% of chord length (12).
+		# NACA 23112 profile describes an airfoil with design lift coefficient of 0.3 (0.15*2), the point of maximum camber located at 15% chord (5*3)
+		# reflex camber (1), and maximum thickness of 12% of chord length (12).
 
 		# coeficient of lift
-		self._cl = int( str( naca_number )[ 0 ] ) * 3 / 2 / 10
+		self._cl = int( str( naca_number )[ 0 ] ) * 3.0 / 2.0 / 10.0 # 0.3
 
 		# point of maximum camber
-		self._p = float( '%.2f' % ( int( str( naca_number ) [ 1 : 3 ] ) / 2 / 100 ) )
+		self._p = float( '%.3f' % ( int( str( naca_number ) [ 1 : 3 ] ) / 2.0 / 100.0 ) ) #.155
 
 		# maximum thickness as percentage along chord length
-		self._t = int( str( naca_number ) [ 3 : 5 ] )
+		self._t = int( str( naca_number ) [ 3 : 5 ] ) / 100.0 # 0.12
 
-		self._m, self._k1 = self._mean_line_map.get( self._p )
+		self._m, self._k1 = self._mean_line_map.get( self._p ) #.2025, 15.957
 
 
 	def _calculate_points( self ):
 		'''
 		'''
 
-		pairs = [ ]
+		yt = [ 5 * self._t * ( self._a[ 0 ] * math.sqrt( x ) + self._a[ 1 ] * x +self._a[ 2 ] * math.pow( x, 2 ) + self._a[ 3 ] * math.pow( x, 3 ) + self._a[ 4 ] * pow( x, 4 ) ) for x in self._x_points ]
 
-		for x in self._x_points:
-			if x >= 0 and x <= self._m:
-				yc = self._k1 / 6 * ( numpy.power( x, 3 ) - ( 3 * self._m * numpy.power( x, 2 ) ) + ( numpy.power( self._m, 2 ) * ( 3 - self._m ) * x ) )
+		xc1 = [x for x in self._x_points if x <= self._p]
+		xc2 = [x for x in self._x_points if x > self._p]
+		xc = xc1 + xc2
 
-			else:
-				yc = self._k1 * numpy.power( self._m, 3 ) / 6 * ( 1 - x )
+		if self._p == 0:
+			xu = self._x_points
+			yu = yt
 
-			pairs.append( ( x, yc ) )
+			xl = self._x_points
+			yl = [ -x for x in yt ]
 
-		return pairs
+			zc = [ 0 ] * len( xc )
+		else:
+			yc1 = [ self._k1 / 6.0 * ( math.pow( x, 3) - 3 * self._m * math.pow( x, 2 ) + math.pow( self._m, 2 ) * ( 3 - self._m ) * x ) for x in xc1 ]
+			yc2 = [ self._k1 / 6.0 * math.pow( self._m, 3 ) * ( 1 - x ) for x in xc2 ]
+			zc  = [ self._cl / 0.3 * x for x in yc1 + yc2 ]
+
+			dyc1_dx = [ self._cl / 0.3 * ( 1.0 / 6.0 ) * self._k1 * ( 3 * math.pow( x, 2 ) - 6 * self._m * x + math.pow( self._m, 2) * ( 3 - self._m ) ) for x in xc1 ]
+			dyc2_dx = [ self._cl / 0.3 * ( 1.0 / 6.0 ) * self._k1 * math.pow( self._m, 3 ) ] *len( xc2 )
+
+			dyc_dx = dyc1_dx + dyc2_dx
+			th = [ numpy.arctan( x ) for x in dyc_dx ]
+
+			xu = [ x - y * numpy.sin( z ) for x, y, z in zip( self._x_points, yt, th ) ]
+			yu = [ x + y * numpy.cos( z ) for x, y, z in zip( zc, yt, th ) ]
+
+			xl = [ x + y * numpy.sin( z ) for x, y, z in zip( self._x_points, yt, th ) ]
+			yl = [ x - y * numpy.cos( z ) for x, y, z in zip( zc, yt, th ) ]
+
+
+		X = xu[ ::-1 ] + xl[ 1: ]
+		Z = yu[ ::-1 ] + yl[ 1: ]
+
+		return X, Z
+
+
+	def _mean_camber_line( self ):
+		pass
 
 
 
@@ -248,15 +275,16 @@ if __name__ == '__main__':
 
 	matplotlib.pyplot.plot( naca_4.x_points, naca_4.mean_camber_line, 'r' )
 	matplotlib.pyplot.axis( 'equal' )
+	matplotlib.pyplot.grid( True )
 	matplotlib.pyplot.xlim( ( -0.05, 1.05 ) )
 	matplotlib.pyplot.show( )
 
 
-	# naca_5 = NACA_5( 23112 )
-	# for point in naca_5.points:
-	# 	print( point )
-	# 	matplotlib.pyplot.plot( point[ 0 ], point[ 1 ], 'b' )
+	naca_5 = NACA_5( 23112, cosine_spacing = False )
+	matplotlib.pyplot.plot( naca_5.points[ 0 ], naca_5.points[ 1 ], 'b' )
+	#matplotlib.pyplot.plot( naca_5.x_points, naca_5.mean_camber_line, 'r' )
 
-	# matplotlib.pyplot.axis( 'equal' )
-	# matplotlib.pyplot.xlim( ( -0.05, 1.05 ) )
-	# matplotlib.pyplot.show( )
+	matplotlib.pyplot.axis( 'equal' )
+	matplotlib.pyplot.grid( True )
+	matplotlib.pyplot.xlim( ( -0.05, 1.05 ) )
+	matplotlib.pyplot.show( )
